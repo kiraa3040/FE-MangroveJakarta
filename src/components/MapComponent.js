1ddbc3d68817b3aa2cf1f0d9b88306a28f8ab4a4
Najoan <najoanrizki@gmail.com>
2026-03-18 23:36:25 +0700

    change leaflet pin

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -8,12 +8,9 @@ import L from "leaflet";
 // === FIX BUG IKON MARKER GAIB DI NEXT.JS ===
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+  iconUrl: "/leaflet/marker-icon.png",
+  shadowUrl: "/leaflet/marker-shadow.png",
 });
 
 export default function MapComponent({ locations = [] }) {
